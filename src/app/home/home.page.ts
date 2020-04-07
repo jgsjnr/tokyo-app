5b1ef22d0df0b6f54a33c51f207a2f8144c3d817
José <josegdasilvajunior@gmail.com>
2020-04-07 03:26:54 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage { 
+  constructor(private httpClient:HttpClient) {    
+  }
+   getProfileOn(){
+     this.httpClient.get('http://192.168.4.1:80/H').subscribe(data=>{console.log(data)});//process the json data   
+   }
+   getProfileOff(){
+     this.httpClient.get('http://192.168.4.1:80/L').subscribe(data=>{console.log(data)});
+   }
 }
